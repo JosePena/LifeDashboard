f857c2de0cf3cf3285406cd6778d958606eceeb5
JosePena <jl.pena.fernandez@gmail.com>
2013-10-06 17:20:20 +0200

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifeDashboard::Application.config.secret_key_base = 'd358946dd0abf911b7f701b5f207724e3c139ed2643a60f1fe927b027392fd2b5c8ce9e2583d83b7cdd416d472db17fdb5352306da63cbc20fa0baac268a59bd'
